b5029588324de435a48529089749b444c26be49e
KimJinWoong <142071404+KJirung@users.noreply.github.com>
2024-02-01 12:36:36 +0900

    Add files via upload

---
+++ C/2839.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,cnt=0;
+
+    scanf("%d",&n);
+
+    while(n>=0){
+        if(n%5==0){
+            cnt+=(n/5);
+            printf("%d\n",cnt);
+            return 0;
+        }
+        n-=3;
+        cnt++;
+    }
+    printf("-1\n");
+
+
+    return 0;
+}
